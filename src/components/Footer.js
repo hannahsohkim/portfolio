4a64a8ef703d0af913443dd5136473da3ebbfbf5
Hannah Kim <hannahsohkim@gmail.com>
2019-05-10 17:55:41 -0700

    Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Icon = styled.i`
+  color: grey;
+  &:hover {
+    text-shadow: 1px 1px 1px #ccc;
+  }
+`
+
+const Footer = () => {
+  return (
+    <div>
+      <a href="mailto:hannahsohkim@gmail.com" target="_blank" rel="noopener noreferrer">
+        <Icon className="fa fa-envelope fa-fw fa-3x" />
+      </a>
+      <a href="http://www.linkedin.com/in/hannah-sohyoung-kim" target="_blank" rel="noopener noreferrer">
+        <Icon className="fab fa-linkedin fa-fw fa-3x" />
+      </a>
+      <a href="https://github.com/hannahsohkim" target="_blank" rel="noopener noreferrer">
+        <Icon className="fab fa-github fa-fw fa-3x" />
+      </a>
+      <a href="https://medium.com/@hannahsohkim" target="_blank" rel="noopener noreferrer">
+        <Icon className="fab fa-medium fa-fw fa-3x" />
+      </a>
+      <br />
+      <p>© 2019 Hannah Kim</p>
+    </div>
+  )
+}
+
+export default Footer;
