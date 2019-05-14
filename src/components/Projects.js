5b6b737f3269ab8bedbe89555204eceeb083a586
Hannah Kim <hannahsohkim@gmail.com>
2019-05-14 01:54:36 -0700

    Refactor projects to use modals

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Label, Card, Image } from 'semantic-ui-react'
+import { Card } from 'semantic-ui-react'
+import Bookshelf from './Bookshelf'
+import Bandland from './Bandland'
+import ContactManager from './ContactManager'
 
-const StyledLink = styled.a`
-  text-decoration: none;
-  color: black;
-  &:focus, &:hover {
-    color: #2f4f4f
-    text-shadow: 1px 1px 1px #ccc;
-  }
-  padding: 10px;
-`
 
 const Container = styled.div`
   margin-left: 100px;
   margin-right: 100px;
-`
-
-const StyledImage = styled(Image)`
-  padding-bottom: 40px;
-  padding-top: 10px
 `
 
 const Intro = styled.p`
@@ -28,68 +17,86 @@ const Intro = styled.p`
   font-size: 18px
 `
 
-const Wrapper = styled.div`
-  padding-bottom: 20px
-`
 
-const Projects = () => (
-  <Container>
-    <Intro>Here are some applications I've worked on!</Intro>
-    <Card.Group className='ui three cards'>
-      <Card style={{width: '400px'}}>
-        <Card.Header style={{fontSize: '20px'}}><strong>BOOKSHELF</strong></Card.Header>
-        <Card.Content>
-        <Wrapper>
-        <Label>React.js</Label>
-          <Label>Node.js</Label>
-          <Label>Express</Label>
-          <Label>MySQL</Label>
-          <Label>Jest</Label>
-          <Label>Enzyme</Label>
-        </Wrapper>
-        <StyledImage className="ui large" alt="Bookshelf" src={require('../assets/bookshelf.jpg')} />
-        <Card.Meta><StyledLink href="https://github.com/hannahsohkim/Bookshelf-Reviews" target="_blank" rel="noopener noreferrer">GITHUB REPO</StyledLink></Card.Meta>
-        <br/>
-        <Card.Description>Bookshelf is a social catalogue and database for books. Users can review and rate books, find books or authors, and interact with one another. I was tasked with the reviews service, a full stack microservice that leverages React to conditionally render and filter reviews by rating. At the end, my team members and I brought together our individual microservices into an integrated applicaton.</Card.Description>
-        </Card.Content>
-      </Card>
+class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.bookshelfToggle = this.bookshelfToggle.bind(this);
+    this.bandlandToggle = this.bandlandToggle.bind(this);
+    this.contactToggle = this.contactToggle.bind(this);
+  }
 
-      <Card style={{width: '400px'}}>
-        <Card.Header style={{fontSize: '20px'}}><strong>BANDLAND</strong></Card.Header>
-        <Card.Content>
-        <Wrapper>
-          <Label>Node.js</Label>
-          <Label>Express</Label>
-          <Label>PostgreSQL</Label>
-          <Label>Artillery</Label>
-        </Wrapper>
-        <StyledImage alt="Bandland" src={require('../assets/bandland.jpg')} />
-        <Card.Meta><StyledLink href="https://github.com/hannahsohkim/Bandland-Recommended" target="_blank" rel="noopener noreferrer">GITHUB REPO</StyledLink></Card.Meta>
-        <br/>
+  state = {
+    bookshelf: true,
+    bandland: true,
+    contact: true
+  }
 
-        <Card.Description>Bandland is a platform for artists to share their music and for fans to discover new music and support their favorite artists. I was tasked with taking a legacy codebase and reworking the backend to handle higher traffic and data. I tested and benchmarked two databases, PostgreSQL and Cassandra, and decided to redesign the backend using PostgreSQL. I generated and seeded 10M+ records, optimized query times to be under 1ms, and stress tested the app to handle 1000 requests per second.</Card.Description>
-        </Card.Content>
-      </Card>
+  bookshelfToggle = () => {
+    this.setState({
+      bookshelf: !this.state.bookshelf
+    })
+  }
 
+  bandlandToggle = () => {
+    this.setState({
+      bandland: !this.state.bandland
+    })
+  }
 
-      <Card style={{width: '400px'}}>
-        <Card.Header style={{fontSize: '20px'}}><strong>CONTACT MANAGER</strong></Card.Header>
-        <Card.Content>
-        <Wrapper>
-          <Label>React.js</Label>
-          <Label>React Context API</Label>
-          <Label>Semantic UI</Label>
-        </Wrapper>
-        <StyledImage alt="Contact Manager" src={require('../assets/contactmanager.jpg')} />
-        <Card.Meta><StyledLink href="https://github.com/hannahsohkim/contact-manager" target="_blank" rel="noopener noreferrer">GITHUB REPO</StyledLink></Card.Meta>
-        <br/>
+  contactToggle = () => {
+    this.setState({
+      contact: !this.state.contact
+    })
+  }
 
-        <Card.Description>A simple contact management app that allows a user to view all contacts, add a new contact, edit an existing contact, and delete a contact. I created this project to experiment with React's Context API as a state management solution and to play with Semantic UI's styling. The add, delete, and edit functionalities all have a pop up modal.</Card.Description>
-        </Card.Content>
-      </Card>
-    </Card.Group>
-  </Container>
-)
+  render () {
+    const { bookshelfToggle, bandlandToggle, contactToggle } = this;
+    const { bookshelf, bandland, contact } = this.state;
+
+    if (!bookshelf) {
+      console.log('bookshelf')
+      return (
+        <Bookshelf toggle={bookshelfToggle}/>
+      )
+    } else if (!bandland) {
+      return (
+        <Bandland toggle={bandlandToggle} />
+      )
+    } else if (!contact) {
+      return (
+        <ContactManager toggle={contactToggle} />
+      )
+    }
+
+    return (
+      <Container>
+        <Intro>Here are some applications I've worked on! Please <strong><em>click</em></strong> on a project to find out more.</Intro>
+        <Card.Group className='ui three cards'>
+          <Card onClick={bookshelfToggle} style={{color: '#696969'}}>
+            <Card.Header style={{color: '#434343'}}><strong>Bookshelf</strong></Card.Header>
+            <Card.Content>
+              Bookshelf is a full stack application, a social catalogue and database for books.
+            </Card.Content>
+          </Card>
+          <Card onClick={bandlandToggle} style={{color: '#696969'}}>
+            <Card.Header style={{color: '#434343'}}><strong>Bandland</strong></Card.Header>
+            <Card.Content>
+              Bandland is a platform for artists to share their music and for fans to support them.
+            </Card.Content>
+          </Card>
+          <Card onClick={contactToggle} style={{color: '#696969'}}>
+            <Card.Header style={{color: '#434343'}}><strong>Contact Manager</strong></Card.Header>
+            <Card.Content>
+              Contact Manager is a simple management app that allows users to keep track of their contacts.
+            </Card.Content>
+          </Card>
+        </Card.Group>
+      </Container>
+    )
+}
+}
+
 
 
 export default Projects;
